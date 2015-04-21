d59d80ea27377607427ffc771f8384af17c14d4c
Marco Scannadinari <m@scannadinari.co.uk>
2015-04-21 22:49:09 +0100

    initial migration to jspm/skatejs

---
+++ config.js
@@ -0,0 +1,35 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm-pkg/github/*.js",
+    "npm:*": "jspm-pkg/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.1.10",
+    "babel-runtime": "npm:babel-runtime@5.1.10",
+    "core-js": "npm:core-js@0.8.4",
+    "skatejs": "npm:skatejs@0.13.2",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:core-js@0.8.4": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:skatejs@0.13.2": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
+  }
+});
+
