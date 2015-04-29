51c67503421358665832ecb32e26fe990bfe6db6
Marco Scannadinari <m@scannadinari.co.uk>
2015-04-29 19:25:36 +0100

    CalmBar.sticky -> CalmBar.bar-sticky

--- els/calm-bar.js
+++ els/calm-bar.js
@@ -8,22 +8,27 @@ export default skate("calm-bar", {
 				:host {
 					display: flex;
 					align-items: stretch;
+
 					width: 100%;
 					height: 56px;
+
 					line-height: 56px;
+
 					box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 				}
 
-				:host(.sticky) {
+				:host(.bar-sticky) {
 					position: fixed;
 					top: 0;
 					left: 0;
+
 					z-index: 97;
 				}
 
 				::content > header {
-					font-size: 20px;
 					margin-left: 16px;
+
+					font-size: 20px;
 				}
 			</style>
 
