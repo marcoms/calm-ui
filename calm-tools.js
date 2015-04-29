67e97a3a51cb9c5bb90ce7c265b30b4cf456175f
Marco Scannadinari <m@scannadinari.co.uk>
2015-04-29 19:21:41 +0100

    add standard transition easings and durations in calm-tools

--- calm-tools.js
+++ calm-tools.js
@@ -8,7 +8,7 @@ export default {
 			e.target.classList.remove("active");
 		}
 
-		let targets = el.shadowRoot.querySelectorAll(".handle-active")
+		let targets = el.shadowRoot.querySelectorAll(".handle-active");
 		Array.prototype.forEach.call(targets, function(target) {
 			if("ontouchstart" in window) {
 				target.addEventListener("touchstart", activate);
@@ -24,5 +24,21 @@ export default {
 
 	init(el, tmpl) {
 		el.createShadowRoot().innerHTML = tmpl;
+	},
+
+	// -quart easings
+
+	ease: {
+		out: "cubic-bezier(0.165, 0.84, 0.44, 1)",
+		in: "cubic-bezier(0.895, 0.03, 0.685, 0.22)",
+		inOut: "cubic-bezier(0.77, 0, 0.175, 1)"
+	},
+
+	// durations for transitions
+
+	time: {
+		short: "160ms",
+		med: "320ms",
+		long: "420ms"
 	}
 };
