3d7c7df11f1d887e4967c51aa51f7491b640a2fd
Marco Scannadinari <m@scannadinari.co.uk>
2015-04-29 19:27:41 +0100

    add CalmDrawer

--- calm-ui.js
+++ calm-ui.js
@@ -1,8 +1,10 @@
-import CalmBtn from "els/calm-btn";
+import CalmDrawer from "els/calm-drawer";
 import CalmBar from "els/calm-bar";
+import CalmBtn from "els/calm-btn";
 import CalmField from "els/calm-field";
 
 export default {
+	CalmDrawer,
 	CalmBar,
 	CalmBtn,
 	CalmField
---
+++ els/calm-drawer.js
@@ -0,0 +1,79 @@
+import calm from "calm-tools";
+import skate from "skatejs";
+
+export default skate("calm-drawer", {
+	created(el) {
+		calm.init(el, `
+			<style>
+				:host(.drawer-open) #drawer {
+					transform: none;
+					visibility: visible;
+				}
+
+				:host(.drawer-open) #overlay {
+					opacity: 0.4;
+					visibility: visible;
+				}
+
+				:host {
+					height: 100%;
+					width: 320px;
+				}
+
+				#drawer {
+					position: fixed;
+					top: 0;
+					left: 0;
+					z-index: 99;
+
+					height: inherit;
+					width: inherit;
+
+					transform: translateX(-100%);
+					visibility: hidden;
+					background: #fff;
+					box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+					transition: transform ${calm.time.long} ${calm.ease.out}, visibility ${calm.time.long} linear;
+					will-change: transform, visibility;
+				}
+
+				#overlay {
+					position: fixed;
+					top: 0;
+					right: 0;
+					bottom: 0;
+					left: 0;
+					z-index: 98;
+
+					opacity: 0;
+					background: #000;
+					visibility: hidden;
+
+					transition: opacity ${calm.time.long} ${calm.ease.out}, visibility ${calm.time.long} linear;
+				}
+
+				:host(.drawer-no-dim) #overlay {
+					background: transparent;
+				}
+			</style>
+
+			<div id="drawer"><content></content></div>
+			<div id="overlay"></div>
+		`);
+	},
+
+	prototype: {
+		open() {
+			this.classList.add("drawer-open");
+		},
+
+		close() {
+			this.classList.remove("drawer-open");
+		},
+
+		toggle() {
+			this.classList.toggle("drawer-open");
+		}
+	}
+});
