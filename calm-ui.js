9a37d8ab6b37dbdfe6e69c6f036eb0ac40e147f2
Marco Scannadinari <m@scannadinari.co.uk>
2015-05-04 23:19:43 +0100

    calm-ui: export CalmIconBtn

--- calm-ui.js
+++ calm-ui.js
@@ -1,6 +1,7 @@
 import CalmDrawer from "els/calm-drawer";
 import CalmBar from "els/calm-bar";
 import CalmBtn from "els/calm-btn";
+import CalmIconBtn from "els/calm-icon-btn";
 import CalmField from "els/calm-field";
 import CalmIcon from "els/calm-icon";
 
@@ -8,6 +9,7 @@ export default {
 	CalmDrawer,
 	CalmBar,
 	CalmBtn,
+	CalmIconBtn,
 	CalmField,
 	CalmIcon
 };
