49685bddbeb86d999e125c4387ac45737587c8a6
Marco Scannadinari <m@scannadinari.co.uk>
2015-05-29 17:13:07 +0100

    update libs

--- config.js
+++ config.js
@@ -17,18 +17,15 @@ System.config({
   "map": {
     "babel": "npm:babel-core@5.4.7",
     "babel-runtime": "npm:babel-runtime@5.4.7",
-    "core-js": "npm:core-js@0.9.11",
+    "core-js": "npm:core-js@0.9.13",
     "skatejs": "npm:skatejs@0.13.4",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:core-js@0.9.11": {
+    "npm:core-js@0.9.13": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:core-js@0.9.4": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:skatejs@0.13.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
