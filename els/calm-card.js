425eeb03bf3770f361e961cc2b9dae26e645eeef
Marco Scannadinari <m@scannadinari.co.uk>
2015-08-12 19:00:48 +0100

    CalmCard: formatting

--- els/calm-card.js
+++ els/calm-card.js
@@ -20,11 +20,25 @@ export default skate("calm-card", {
 				transition: box-shadow ${calm.time.short} linear;
 			}
 
-			:host([z="1"]) { box-shadow: ${calm.shadow[1]}; }
-			:host([z="2"]) { box-shadow: ${calm.shadow[2]}; }
-			:host([z="3"]) { box-shadow: ${calm.shadow[3]}; }
-			:host([z="4"]) { box-shadow: ${calm.shadow[4]}; }
-			:host([z="5"]) { box-shadow: ${calm.shadow[5]}; }
+			:host([z="1"]) {
+				box-shadow: ${calm.shadow[1]};
+			}
+
+			:host([z="2"]) {
+				box-shadow: ${calm.shadow[2]};
+			}
+
+			:host([z="3"]) {
+				box-shadow: ${calm.shadow[3]};
+			}
+
+			:host([z="4"]) {
+				box-shadow: ${calm.shadow[4]};
+			}
+
+			:host([z="5"]) {
+				box-shadow: ${calm.shadow[5]};
+			}
 		</style>
 
 		<content></content>
