d8d9115a94eef7bf36cf611910c729c77011054e
Marco Scannadinari <m@scannadinari.co.uk>
2015-08-13 13:32:56 +0100

    CalmSpinner: use material design spinner based on spinner.svg from Chromium

--- els/calm-spinner.js
+++ els/calm-spinner.js
@@ -8,41 +8,60 @@ export default skate("calm-spinner", {
 		},
 	},
 
+	// the following template is adapted from throbber.svg (BSD licensed) by the Chromium project
 	template: calm.shadowDom(`
 		<style>
-			@keyframes spin {
-				from { transform: none; }
-				to { transform: rotateZ(360deg); }
+			@keyframes fill-unfill {
+				0% {
+					stroke-dashoffset: 58.8;
+				}
+
+				50% {
+					stroke-dashoffset: 0;
+				}
+
+				100% {
+					stroke-dashoffset: -58.4;
+				}
 			}
 
-			:host {
-				display: block;
-				width: 24px;
-				height: 24px;
+			@keyframes rotate {
+				0% {
+					transform: none;
+				}
 
-				color: ${calm.color};
-				border-color: currentColor;
+				100% {
+					transform: rotate(360deg);
+				}
 			}
 
-			:host([small]) {
-				width: 16px;
-				height: 16px;
+			svg, svg * {
+				transform-origin: 50% 50%;
+			}
+
+			#wrapper {
+				animation: rotate 1411.767ms infinite linear;
 			}
 
 			#spinner {
-				width: 100%;
-				height: 100%;
+				stroke: ${calm.color};
+				stroke-dasharray: 58.9;
+				stroke-dashoffset: 58.9;
+				stroke-width: 2.5;
+				stroke-linecap: round;
 
-				border-radius: 50%;
-				border-color: inherit;
-				border: 2px solid;
-				border-top: 2px solid transparent;
-				border-right: 2px solid transparent;
-
-				animation: spin ${calm.time.long} linear infinite;
+				animation-name: fill-unfill, rotate;
+				animation-duration: 1199.7ms, 4798.8ms;
+				animation-iteration-count: infinite;
+				animation-timing-function: cubic-bezier(0.4, 0.0, 0.2, 1), steps(4);
+				animation-direction: normal, reverse;
 			}
 		</style>
 
-		<div id="spinner"></div>
+		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 0 28 28">
+		  <g id="wrapper">
+		    <path id="spinner" fill="none" d="M 14,1.5 A 12.5,12.5 0 1 1 1.5,14"></path>
+		  </g>
+		</svg>
 	`),
 });
