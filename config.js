de223c880f7e5c0730ec5910b60d4d2db5ecccd4
Marco Scannadinari <m@scannadinari.co.uk>
2015-10-03 21:30:47 +0100

    update libs

--- config.js
+++ config.js
@@ -1,29 +1,27 @@
 System.config({
-  "baseURL": "/",
-  "defaultJSExtensions": true,
-  "transpiler": "babel",
-  "babelOptions": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime"
     ]
   },
-  "paths": {
+  paths: {
     "github:*": "jspm-pkg/github/*",
     "npm:*": "jspm-pkg/npm/*"
-  }
-});
+  },
 
-System.config({
-  "map": {
-    "babel": "npm:babel-core@5.8.20",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
+  map: {
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@0.9.18",
     "dedent": "npm:dedent@0.4.0",
     "skatejs": "github:skatejs/skatejs@master",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@5.8.20": {
+    "npm:babel-runtime@5.8.25": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
@@ -33,4 +31,3 @@ System.config({
     }
   }
 });
-
