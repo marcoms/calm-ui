245b7688641ecbbc00cd833e3da68f4859c2b91f
Marco Scannadinari <m@scannadinari.co.uk>
2015-12-23 15:36:34 +0000

    calm-tools: use only one box-shadow

--- calm-tools.js
+++ calm-tools.js
@@ -59,11 +59,11 @@ export default {
 	// box-shadow parameters for different elevation levels
 
 	shadows: {
-		1: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
-		2: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)",
-		3: "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)",
-		4: "0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)",
-		5: "0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.22)",
+		1: "0 1px 4px rgba(0, 0, 0, 0.3)",
+		2: "0 2px 8px rgba(0, 0, 0, 0.35)",
+		3: "0 4px 16px rgba(0, 0, 0, 0.4)",
+		4: "0 8px 24px rgba(0, 0, 0, 0.45)",
+		5: "0 16px 42px rgba(0, 0, 0, 0.5)",
 	},
 
 	// TODO: implement as CSS custom properties when support is finalised... Come on https://codereview.chromium.org/1192983003/!
